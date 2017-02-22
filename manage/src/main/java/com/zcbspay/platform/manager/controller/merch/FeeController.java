a4523c284d0336daad4c3da5f25249d890ece535
bema <bema@DESKTOP-NTQ29PF>
2017-02-22 10:29:58 +0800

    常规扣率,卡扣率

--- manage/src/main/java/com/zcbspay/platform/manager/controller/merch/FeeController.java
+++ manage/src/main/java/com/zcbspay/platform/manager/controller/merch/FeeController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.zcbspay.platform.manager.merch.bean.BusiRateEntity;
 import com.zcbspay.platform.manager.merch.bean.CardRateEntity;
+import com.zcbspay.platform.manager.merch.bean.CommonRateEntity;
+import com.zcbspay.platform.manager.merch.bean.NewCardRateEntity;
 import com.zcbspay.platform.manager.merch.service.FeeService;
 import com.zcbspay.platform.manager.utils.JsonUtils;
 import com.zcbspay.platform.manager.utils.UserHelper;
@@ -27,14 +29,39 @@ public class FeeController {
 	@Autowired
 	private FeeService feeService;
 	
+	/**
+	 * 业务类型扣率
+	 * @return
+	 */
 	@RequestMapping("showBusiRate")
 	public String showBusiRate() {
 		return "fee/txn_rate_manager";
 	}
-
+	/**
+	 * 卡类型扣率
+	 * @return
+	 */
 	@RequestMapping("showCardRate")
 	public String showCardRate() {
 		return "fee/card_rate_manager";
+	}
+	
+	/**
+	 * 常规类型扣率
+	 * @return
+	 */
+	@RequestMapping("showCommonRate")
+	public String showCommonRate() {
+		return "fee/_rate_common_manager";
+	}
+	
+	/**
+	 * 新版卡扣率
+	 * @return
+	 */
+	@RequestMapping("showNewCardRate")
+	public String showNewCardRate() {
+		return "fee/_rate_card_manager";
 	}
 
 	/**
@@ -65,6 +92,25 @@ public class FeeController {
 		Map<String, Object> busiList = feeService.findBusiRateByPage(variables, page, rows);
 		return busiList;
 	}
+	
+	
+	
+	/**
+	 * 常规扣率版本分页查询
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("queryCommonRate")
+	public Map<String, Object> queryCommonRate(HttpServletRequest request,CommonRateEntity rateEntity, String page,
+			String rows) {
+		Map<String, Object> variables = new HashMap<String, Object>();
+		variables.put("userId", UserHelper.getCurrentUser(request).getUserId());
+		variables.put("rateId", rateEntity.getRateId());
+		variables.put("rateDes", rateEntity.getRateDes());
+		Map<String, Object> busiList = feeService.findCommonRateByPage(variables, page, rows);
+		return busiList;
+	}
 
 	/**
 	 * 扣率版本下的业务实例
@@ -87,6 +133,18 @@ public class FeeController {
 	@RequestMapping("queryOneBusiRate")
 	public Map<String, Object> queryOneBusiRate(String caseid) {
 		Map<String, Object> feecase = feeService.queryOneBusiRate(caseid);
+		return feecase;
+	}
+	
+	/**
+	 * 查询一条常规扣率版本实例信息
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("queryOneCommonRate")
+	public Map<String, Object> queryOneCommonRate(String caseid) {
+		Map<String, Object> feecase = feeService.queryOneCommonRate(caseid);
 		return feecase;
 	}
 
@@ -112,6 +170,33 @@ public class FeeController {
 		String mark = feeService.updateBusiRate(busiRateEntity);
 		JsonUtils.json_encodeAndWrite(response, mark);
 	}
+	
+	
+	/**
+	 * 修改扣率实例
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("updateCommonRate")
+	public void updateCommonRate(CommonRateEntity rateEntity, HttpServletResponse response) {
+		if (!StringUtils.isEmpty(rateEntity.getMinFeeStr())) {
+			rateEntity.setMinFee(new BigDecimal(rateEntity.getMinFeeStr()));
+		}
+
+		if (!StringUtils.isEmpty(rateEntity.getMaxFeeStr())) {
+			rateEntity.setMaxFee(new BigDecimal(rateEntity.getMaxFeeStr()));
+		}
+
+		if (!StringUtils.isEmpty(rateEntity.getFeeRateStr())) {
+			rateEntity.setFeeRate(new BigDecimal(rateEntity.getFeeRateStr()));
+		}
+		String mark = feeService.updateCommonRate(rateEntity);
+		JsonUtils.json_encodeAndWrite(response, mark);
+	}
+	
+	
+	
 
 	/**
 	 * 添加扣率实例
@@ -138,6 +223,33 @@ public class FeeController {
 		String mark = feeService.addOneBusiRate(busiRateModel);
 		JsonUtils.json_encodeAndWrite(response, mark);
 	}
+	
+	
+	/**
+	 * 添加常规扣率
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("saveCommonRate")
+	public void saveCommonRate(CommonRateEntity rateModel, HttpServletRequest request, HttpServletResponse response) {
+
+		if (!StringUtils.isEmpty(rateModel.getMinFeeStr())) {
+			rateModel.setMinFee(new BigDecimal(rateModel.getMinFeeStr()));
+		}
+
+		if (!StringUtils.isEmpty(rateModel.getMaxFeeStr())) {
+			rateModel.setMaxFee(new BigDecimal(rateModel.getMaxFeeStr()));
+		}
+
+		if (!StringUtils.isEmpty(rateModel.getFeeRateStr())) {
+			rateModel.setFeeRate(new BigDecimal(rateModel.getFeeRateStr()));
+		}
+
+		rateModel.setInuser(UserHelper.getCurrentUser(request).getUserId().toString());
+		String mark = feeService.addOneCommonRate(rateModel);
+		JsonUtils.json_encodeAndWrite(response, mark);
+	}
 
 	/**
 	 * 卡扣率分页查询
@@ -156,6 +268,21 @@ public class FeeController {
 		Map<String, Object> busiList = feeService.findCardRateByPage(variables, page, rows);
 		return busiList;
 	}
+	
+	
+	/**
+	 * 新卡扣率分页查询
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("queryNewCardRate")
+	public Map<String, Object> queryNewCardRate(NewCardRateEntity cardRateEntity, String page, String rows,
+			HttpServletRequest request, HttpServletResponse response) {
+		cardRateEntity.setInuser(UserHelper.getCurrentUser(request).getUserId());
+		Map<String, Object> busiList = feeService.findNewCardRateByPage(cardRateEntity, page, rows);
+		return busiList;
+	}
 
 	/**
 	 * 查询一条卡扣率版本实例信息
@@ -166,6 +293,19 @@ public class FeeController {
 	@RequestMapping("queryOneCardRate")
 	public Map<String, Object> queryOneCardRate(String caseid) {
 		Map<String, Object> feecase = feeService.queryOneCardRate(caseid);
+		return feecase;
+	}
+	
+	
+	/**
+	 * 查询一条新卡类型扣率版本实例信息
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("queryOneNewCardRate")
+	public Map<String, Object> queryOneNewCardRate(String caseid) {
+		Map<String, Object> feecase = feeService.queryOneNewCardRate(caseid);
 		return feecase;
 	}
 
@@ -217,6 +357,30 @@ public class FeeController {
 		String mark = feeService.addOneCardRate(cardRateEntity);
 		JsonUtils.json_encodeAndWrite(response, mark);
 	}
+	
+	/**
+	 * 添加卡扣率信息
+	 * 
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("saveNewCardRate")
+	public void saveNewCardRate(NewCardRateEntity cardRateEntity, HttpServletRequest request, HttpServletResponse response) {
+		cardRateEntity.setInuser(UserHelper.getCurrentUser(request).getUserId());
+		String mark = feeService.saveNewCardRate(cardRateEntity);
+		JsonUtils.json_encodeAndWrite(response, mark);
+	}
 
+	/**
+	 * 查询扣率类型
+	 * @return
+	 */
+	@ResponseBody
+	@RequestMapping("queryRateType")
+	public List<?> queryRateType() {
+		List<?> resultList = feeService.queryRateType();
+		return resultList;
+	}
+	
 	
 }
